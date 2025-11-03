a26f5cb71963c5993f9b99f2912d0d67d67f6492
Eric <eriiic-15@hotmail.com>
2025-11-04 00:17:04 +0100

    E:solució p2

--- js/funcions.js
+++ js/funcions.js
@@ -1,3 +1,7 @@
 function confirmaRegistre(){
-    //completa
+    alert("Registrant estudiant");
+    console.log("Registrant estudiant");
+    let form = document.getElementById("formDiv");
+    form.innerHTML = `<p class="important">T'has registrat amb èxit</p>`
+    return false;
 }
